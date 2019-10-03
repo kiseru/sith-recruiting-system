5670c865c8061ee229904a3e55ca2c50932c1ab8
Kiselev Alexandr <kis.alex97@gmail.com>
2019-10-03 21:51:36 +0300

    Added form for registering recruit

--- recruiting_system/urls.py
+++ recruiting_system/urls.py
@@ -4,4 +4,5 @@ from recruiting_system import views
 
 urlpatterns = [
     path('siths/', views.SithListView.as_view(), name='sith_list'),
+    path('recruits/new', views.RecruitCreateView.as_view(), name='recruit_create'),
 ]
--- recruiting_system/views.py
+++ recruiting_system/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from recruiting_system import models
@@ -5,3 +6,14 @@ from recruiting_system import models
 
 class SithListView(generic.ListView):
     model = models.Sith
+
+
+class RecruitCreateView(generic.CreateView):
+    model = models.Recruit
+    fields = (
+        'name',
+        'email',
+        'age',
+        'habitat_planet'
+    )
+    success_url = reverse_lazy('recruit_create')
